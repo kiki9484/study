0dda7ba968a96d1ac34fc16d651bf6bd7e64cc55
pug9483 <2019136060@koreatech.ac.kr>
2024-02-19 14:46:59 +0900

    Update JpashopApplication.java
    깃헙 연동 테스트용 커밋

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -8,7 +8,7 @@ public class JpashopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
-		System.out.println("aaaa");
+		System.out.println("깃헙 테스트용 코드");
 	}
 
 }
