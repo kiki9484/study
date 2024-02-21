627967ecd16fb50becdcf787619615e8c12c967d
pug9483 <67894738+pug9483@users.noreply.github.com>
2024-02-21 11:43:48 +0900

    MemberRepositoryTest 코드 작성

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@RunWith(SpringRunner.class) // JUnit에게 스프링 관련 테스트를 한다고 알려주는 애노테이션
+@SpringBootTest
+@Rollback(false) // 롤백을 하지 않고, 테스트 결과를 보고 싶을 때 사용
+public class MemberRepositoryTest {
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        // then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+}
