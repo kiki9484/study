cf3710fbf68aefc46616f7f5c3fe19751da27241
pug9483 <2019136060@koreatech.ac.kr>
2024-02-22 01:01:21 +0900

    모든 연관관계를 지연로딩으로 설정
    @XtoOne(fetch = FetchType.LAZY)로 설정한다.

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -17,13 +17,13 @@ public class Category {
 
     private String name;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "category_item",
             joinColumns = @JoinColumn(name = "category_id"),
             inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Item> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
