ffadbe9697663e0bafd303b4ad5170070f18881a
pug9483 <67894738+pug9483@users.noreply.github.com>
2024-02-22 12:05:30 +0900

    MemberService 생성
     1. 회원 가입
    2. 회원 조회

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -3,18 +3,23 @@ package jpabook.jpashop.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
-    // 스프링이 JPA EntityManager를 자동으로 주입시켜준다.
-    @PersistenceContext
-    private EntityManager em;
+    /*
+     * // @PersistenceContext private EntityManager em;
+     * 스프링 부트 라이브러리를 사용하면 @PersistenceContext를 @Autowired로 바꿀 수 있다.
+     * 그러므로 맴버변수를 final로 바꾸고, @RequiredArgsConstructor를 넣어줄 수 있다.
+     */
+    private final EntityManager em;
 
     public void save(Member member) {
-        em.persist(member);
+        em.persist(member); // 영속성 컨텍스트에 member가 올라간다.
     }
 
     public Member findOne(Long id) {
