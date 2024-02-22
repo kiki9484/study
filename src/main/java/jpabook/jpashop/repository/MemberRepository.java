02f67b8559deed395f319d171c8834a34083c3f2
pug9483 <67894738+pug9483@users.noreply.github.com>
2024-02-22 11:32:40 +0900

    MemberRepository 생성
    메서드 추가
    1. save()
    2. findOne()
    3. findAll()
    4. findByName()

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+    // 스프링이 JPA EntityManager를 자동으로 주입시켜준다.
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    // jpql 작성 - 엔티티 객체를 대상으로 쿼리를 한다.
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
