787ba378dd812cdbdc84658d161be8eff2d98832
pug9483 <67894738+pug9483@users.noreply.github.com>
2024-02-22 16:47:47 +0900

    Item 비즈니스 로직 메서드 생성
    1. addStock(): 재고 수량 더하기
    2. removeStock(): 재고 수량 줄이기(음수가 나올 경우 NotEnoughException 런타임 에러 메시지 던지기)

--- src/main/java/jpabook/jpashop/domain/Item/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.Item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,4 +24,22 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /*
+     * 도메인 주도 설계를 할 때, 엔터티 자체에서 해결할 수 있으면 엔터티 안에 넣는 것이 좋다(객체지향적 사고)
+     * 1. addStock(int quantity): 재고 수량 더하기
+     * 2. removeStock(int quantity): 재고 수량 줄이기
+     */
+
+    // stock 증가 메서드
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    // stock 감소 메서드
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity -= quantity;
+        if(restStock < 0) throw new NotEnoughException("need more stock");
+        this.stockQuantity -= quantity;
+    }
 }
