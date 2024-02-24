c1f5ee0cb83b5798bd7c704cb4e555d699146994
pug9483 <67894738+pug9483@users.noreply.github.com>
2024-02-24 14:50:42 +0900

    상품 수정 - POST

--- src/main/java/jpabook/jpashop/domain/Item/Book.java
+++ src/main/java/jpabook/jpashop/domain/Item/Book.java
@@ -16,6 +16,17 @@ public class Book extends Item{
     private String isbn;
 
     // 생성 메서드
+    public static Book createBook(Long id, String name, int price, int stockQuantity, String author, String isbn){
+        Book book = new Book();
+        book.setId(id);
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+        book.setAuthor(author);
+        book.setIsbn(isbn);
+        return book;
+    }
+
     public static Book createBook(String name, int price, int stockQuantity, String author, String isbn){
         Book book = new Book();
         book.setName(name);
