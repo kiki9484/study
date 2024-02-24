044ef53f09894d5939d7081c39a054213b5c33ba
pug9483 <67894738+pug9483@users.noreply.github.com>
2024-02-24 15:37:51 +0900

    dirty checking과 merge
    준영속 상태일 때, 업데이트 하는 방법
    -> merge보단 dirty checking방법을 써라!

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.Item.Book;
 import jpabook.jpashop.domain.Item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,9 +20,21 @@ public class ItemService {
         itemRepository.save(item);
     }
 
-    public Item findOne(Long itemId) {
-        return itemRepository.findOne(itemId);
-    }
+    /*
+     * 변경 감지 방법(dirty checking) - 이 방법 추천!
+     * itemRepository.save(), merge, entityManager.persist() 모두 호출할 필요가 없다.
+     * findItem으로 찾아온 Item은 영속 상태이다.
+     * 값을 세팅한 다음엔 Transaction이 commit이 돼서 JPA가 flush()를 한다.
+     * JPA는 flush()를 통해 엔터티 컨텍스트에서 변경된 것을 모두 찾는다. 찾아서 바뀐 값을 업데이트해서 db를 변경한다.
+     */
+    /*@Transactional
+    public Item update(Long itemId, Book param) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(param.getPrice());
+        findItem.setName(param.getName());
+        findItem.setStockQuantity(param.getStockQuantity());
+        return findItem;
+    }*/
 
     public List<Item> findItems() {
         return itemRepository.findAll();
