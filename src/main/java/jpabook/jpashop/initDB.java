1ca3ca98c832d9fc0900ed4662d44a56f82a5c93
pug9483 <2019136060@koreatech.ac.kr>
2024-02-27 03:11:05 +0900

    조회용 샘플 데이터 생성

---
+++ src/main/java/jpabook/jpashop/initDB.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.*;
+import jpabook.jpashop.domain.Item.Book;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class initDB {
+
+    private final InitService initService;
+
+    @PostConstruct
+    public void init() {
+        initService.dbInit1();
+    }
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService{
+        private final EntityManager em;
+
+        public void dbInit1() {
+            Member member = new Member();
+            member.setName("userA");
+            member.setAddress(new Address("seoul", "1", "1111"));
+            em.persist(member);
+
+            Book book1 = Book.createBook("JPA1 BOOK", 10000, 100, "park", "1234");
+            em.persist(book1);
+            Book book2 = Book.createBook("JPA2 BOOK", 20000, 100, "park", "1234");
+            em.persist(book2);
+
+            OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);
+            OrderItem orderItem2 = OrderItem.createOrderItem(book2, 20000, 2);
+
+            Delivery delivery = new Delivery();
+            delivery.setAddress(member.getAddress());
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+            em.persist(order);
+        }
+    }
+}
