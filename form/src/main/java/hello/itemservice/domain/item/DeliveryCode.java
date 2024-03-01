eed7473ad7c3ed5b1d74aa18a255db56b256038c
pug9483 <141905308+kiki9484@users.noreply.github.com>
2024-03-01 20:18:10 +0900

    FormItemController
    타임리프와 스프링을 사용하여 HTML Form 객체 다루기

--- form/src/main/java/hello/itemservice/domain/item/DeliveryCode.java
+++ form/src/main/java/hello/itemservice/domain/item/DeliveryCode.java
@@ -11,8 +11,6 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class DeliveryCode {
-
-    private String code;
-    private String displayName;
-
+    private String code; // 시스템에서 전달하는 값
+    private String displayName; // 고객에게 보여주는 값
 }
