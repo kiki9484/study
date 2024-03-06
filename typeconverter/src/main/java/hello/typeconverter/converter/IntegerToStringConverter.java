c462990a381a4f87e728379e5bb395758174386b
pug9483 <2019136060@koreatech.ac.kr>
2024-03-06 15:07:18 +0900

    create converter readme.md

--- typeconverter/src/main/java/hello/typeconverter/converter/IntegerToStringConverter.java
+++ typeconverter/src/main/java/hello/typeconverter/converter/IntegerToStringConverter.java
@@ -6,7 +6,7 @@ import org.springframework.core.convert.converter.Converter;
 @Slf4j
 public class IntegerToStringConverter implements Converter<Integer, String> {
 
-    @Override
+    @Override 
     public String convert(Integer source) {
         log.info("convert source={}", source);
         return String.valueOf(source);
