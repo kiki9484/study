b442cc3e21fa802d0bca06b8904ffbeec145d963
pug9483 <141905308+kiki9484@users.noreply.github.com>
2024-03-30 19:15:05 +0900

    재고보다 많은 수량으로 차감 시도하는 경우 예외가 발생한다.

--- cafekiosk/src/test/java/sample/cafekiosk/domain/stock/StockTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/domain/stock/StockTest.java
@@ -4,11 +4,42 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 
 class StockTest {
     @DisplayName("재고의 수량이 제공된 수량보다 적은지 확인한다.")
     @Test
     void isQuantityLessThan() {
-        Assertions.assertThat(Stock.create("001", 1).isQuantityLessThan(2)).isTrue();
+        assertThat(Stock.create("001", 1).isQuantityLessThan(2)).isTrue();
+    }
+
+    @DisplayName("재고를 주어진 개수만큼 차감할 수 있다.")
+    @Test
+    void deductQuantity1() {
+        // given
+        Stock stock = Stock.create("001", 1);
+        int quantity = 1;
+
+        // when
+        stock.deductQuantity(quantity);
+
+        // then
+        assertThat(stock.getQuantity()).isZero();
+    }
+
+    @DisplayName("재고보다 많은 수량으로 차감 시도하는 경우 예외가 발생한다.")
+    @Test
+    void deductQuantity2() {
+        // given
+        Stock stock = Stock.create("001", 1);
+        int quantity = 2;
+
+
+        // when // then
+        assertThatThrownBy(() -> stock.deductQuantity(quantity))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("차감할 재고 수량이 없습니다.");
+
     }
 }
