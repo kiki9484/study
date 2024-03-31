f100b3d24ac7274b82190a1a24b3786aa82b139d
pug9483 <141905308+kiki9484@users.noreply.github.com>
2024-03-31 12:06:02 +0900

    productNumber 부여하기

--- cafekiosk/src/main/java/sample/cafekiosk/api/controller/product/ProductController.java
+++ cafekiosk/src/main/java/sample/cafekiosk/api/controller/product/ProductController.java
@@ -2,7 +2,9 @@ package sample.cafekiosk.api.controller.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sample.cafekiosk.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.api.service.product.ProductService;
 import sample.cafekiosk.api.service.product.response.ProductResponse;
 
@@ -13,8 +15,13 @@ import java.util.List;
 public class ProductController {
     private final ProductService productService;
 
+    @PostMapping("/api/v1/products/new")
+    public void createProduct(ProductCreateRequest request) {
+        productService.createProduct(request);
+    }
     @GetMapping("/api/v1/products/selling")
     public List<ProductResponse> getSellingProducts() {
         return productService.getSellingProducts();
     }
+
 }
--- cafekiosk/src/test/java/sample/cafekiosk/domain/product/ProductRepositoryTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/domain/product/ProductRepositoryTest.java
@@ -40,6 +40,54 @@ class ProductRepositoryTest {
                 );
     }
 
+    @DisplayName("상품번호 리스트로 상품들을 조회한다.")
+    @Test
+    void findAllByProductNumberIn() {
+        // given
+        Product product1 = createProduct("001", HANDMADE, SELLING, "아메리카노", 4000);
+        Product product2 = createProduct("002", HANDMADE, HOLD, "카페라떼", 4500);
+        Product product3 = createProduct("003", HANDMADE, STOP_SELLING, "팥빙수", 7000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        // when
+        List<Product> products = productRepository.findAllByProductNumberIn(List.of("001", "002"));
+
+        // then
+        assertThat(products).hasSize(2)
+                .extracting("productNumber", "name", "sellingStatus")
+                .containsExactlyInAnyOrder(
+                        tuple("001", "아메리카노", SELLING),
+                        tuple("002", "카페라떼", HOLD)
+                );
+    }
+
+    @DisplayName("가장 마지막으로 저장한 상품의 상품번호를 읽어온다.")
+    @Test
+    void findLatestProductNumber() {
+        // given
+        String targetProductNumber = "003";
+        Product product1 = createProduct("001", HANDMADE, SELLING, "아메리카노", 4000);
+        Product product2 = createProduct("002", HANDMADE, HOLD, "카페라떼", 4500);
+        Product product3 = createProduct(targetProductNumber, HANDMADE, STOP_SELLING, "팥빙수", 7000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        // when
+        String latestProductNumber = productRepository.findLatestProductNumber();
+
+        // then
+        assertThat(latestProductNumber).isEqualTo(targetProductNumber);
+    }
+
+    @DisplayName("가장 마지막으로 저장한 상품의 상품번호를 읽어올 때, 상품이 하나도 없는 경우에는 null을 반환한다.")
+    @Test
+    void findLatestProductNumberWhenProductIsEmpty() {
+        // when
+        String latestProductNumber = productRepository.findLatestProductNumber();
+
+        // then
+        assertThat(latestProductNumber).isNull();
+    }
+
     private Product createProduct(String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price){
         return Product.builder()
                 .productNumber(productNumber)
