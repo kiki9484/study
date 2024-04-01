364080871c7742af48f9c2ca6e8bb5b4393bec3e
pug9483 <141905308+kiki9484@users.noreply.github.com>
2024-04-01 23:32:03 +0900

    해당 일자에 총 합계 내역을 메일로 보내주는 서비스 작성

--- cafekiosk/src/main/java/sample/cafekiosk/domain/order/Order.java
+++ cafekiosk/src/main/java/sample/cafekiosk/domain/order/Order.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 import sample.cafekiosk.domain.BaseEntity;
 import sample.cafekiosk.domain.orderproduct.OrderProduct;
 import sample.cafekiosk.domain.product.Product;
@@ -12,6 +13,7 @@ import sample.cafekiosk.domain.product.Product;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 import static sample.cafekiosk.domain.order.OrderStatus.INIT;
@@ -34,17 +36,18 @@ public class Order extends BaseEntity {
     private List<OrderProduct> orderProducts = new ArrayList<>();
 
     @Builder
-    private Order(List<Product> products, LocalDateTime registeredDateTime) {
-        this.orderStatus = INIT;
+    private Order(List<Product> products, OrderStatus orderStatus, LocalDateTime registeredDateTime) {
+        this.orderStatus = orderStatus;
         this.totalPrice = calculateTotalPrice(products);
         this.registeredDateTime = registeredDateTime;
         this.orderProducts = products.stream()
                 .map(product -> new OrderProduct(this, product))
-                .collect(toList());
+                .collect(Collectors.toList());
     }
 
-    public static Order createOrder(List<Product> products, LocalDateTime registeredDateTime) {
+    public static Order create(List<Product> products, LocalDateTime registeredDateTime) {
         return Order.builder()
+                .orderStatus(INIT)
                 .products(products)
                 .registeredDateTime(registeredDateTime)
                 .build();
