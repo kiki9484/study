ad1fce7b6121523c2b603de55c89c347b0e7841c
pug9483 <2019136060@koreatech.ac.kr>
2024-04-02 11:32:24 +0900

    에러 찾기용 중간 저장

--- cafekiosk/src/test/java/sample/cafekiosk/api/controller/order/OrderCreateRequestTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/api/controller/order/OrderCreateRequestTest.java
@@ -3,6 +3,7 @@ package sample.cafekiosk.api.controller.order;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
@@ -11,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static sample.cafekiosk.domain.product.ProductSellingStatus.HOLD;
 import static sample.cafekiosk.domain.product.ProductSellingStatus.SELLING;
 
+@ActiveProfiles("test")
 class OrderCreateRequestTest {
     @DisplayName("주문 번호를 받아 요청 dto를 생성한다")
     @Test
