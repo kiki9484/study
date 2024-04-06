f28295be30f1a28744735118f425e488646a7b90
pug9483 <141905308+kiki9484@users.noreply.github.com>
2024-04-06 12:31:04 +0900

    ProductControllerDocsTest 작성하기

--- cafekiosk/src/main/java/sample/cafekiosk/domain/history/mail/MailSendHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package sample.cafekiosk.domain.history.mail;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MailSendHistoryRepository extends JpaRepository<MailSendHistory, Long> {
-
-}
