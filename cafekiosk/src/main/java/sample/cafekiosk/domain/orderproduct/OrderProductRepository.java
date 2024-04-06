57eb0d0de8331d0f6a746041c878f06ed80d1664
pug9483 <2019136060@koreatech.ac.kr>
2024-04-06 12:31:04 +0900

    ProductControllerDocsTest 작성하기

--- cafekiosk/src/main/java/sample/cafekiosk/domain/orderproduct/OrderProductRepository.java
+++
@@ -1,7 +0,0 @@
-package sample.cafekiosk.domain.orderproduct;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sample.cafekiosk.domain.order.Order;
-
-public interface OrderProductRepository extends JpaRepository<OrderProduct, Long> {
-}
