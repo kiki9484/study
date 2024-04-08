300814461488cbcb54d28ccc4a1d55156983ee54
pug9483 <2019136060@koreatech.ac.kr>
2024-04-08 14:19:53 +0900

    Spring Data Jpa 학습 테스트 작성

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,17 +18,49 @@ import static org.junit.jupiter.api.Assertions.*;
 @Transactional
 @Rollback(value = false)
 class MemberRepositoryTest {
-    @Autowired MemberRepository memberRepository;
+    @Autowired MemberJpaRepository memberJpaRepository;
 
     @Test
     void testMember() {
         Member member = new Member("memberA");
-        memberRepository.save(member);
+        memberJpaRepository.save(member);
 
-        Member findMember = memberRepository.findById(member.getId()).get();
+        Member findMember = memberJpaRepository.findById(member.getId()).get();
 
         assertThat(findMember).isEqualTo(member);
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+
+    @Test
+    void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        // 단건 조회 검증
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        // 리스트 조회 검증
+        List<Member> members = memberJpaRepository.findAll();
+        assertThat(members).hasSize(2);
+
+        // 카운트 검증
+        assertThat(memberJpaRepository.count()).isEqualTo(2);
+
+        // 수정 검증
+        String newUserName = "member1!!!";
+        member1.setUsername(newUserName);
+        Member findMember = memberJpaRepository.findById(member1.getId()).get();
+        assertThat(findMember.getUsername()).isEqualTo(newUserName);
+
+        // 삭제 검증
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+        assertThat(memberJpaRepository.count()).isEqualTo(0);
     }
 }
