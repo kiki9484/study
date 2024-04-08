9a0b49e66d9eeca45f8f5fc6187daccff39e1938
pug9483 <2019136060@koreatech.ac.kr>
2024-04-08 13:50:55 +0900

    순수 MemberJpaRepository 테스트

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -9,11 +9,13 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
-import static org.assertj.core.api.Assertions.*;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 
 @Transactional
 @SpringBootTest
+@Rollback(value = false)
 class MemberJpaRepositoryTest {
     @Autowired MemberJpaRepository memberJpaRepository;
 
@@ -27,5 +29,37 @@ class MemberJpaRepositoryTest {
         assertThat(findMember).isEqualTo(member); // 동일한 트랜잭션에서 member와 findMember은 같은 인스턴스임을 보장한다.
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+
+    @Test
+    void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        // 단건 조회 검증
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        // 리스트 조회 검증
+        List<Member> members = memberJpaRepository.findAll();
+        assertThat(members).hasSize(2);
+
+        // 카운트 검증
+        assertThat(memberJpaRepository.count()).isEqualTo(2);
+
+        // 수정 검증
+        String newUserName = "member1!!!";
+        member1.setUsername(newUserName);
+        Member findMember = memberJpaRepository.findById(member1.getId()).get();
+        assertThat(findMember.getUsername()).isEqualTo(newUserName);
+
+        // 삭제 검증
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+        assertThat(memberJpaRepository.count()).isEqualTo(0);
     }
 }
