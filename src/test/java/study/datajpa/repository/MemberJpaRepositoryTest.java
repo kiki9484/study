64d9e07fdf465618fe738908438819712e946bc8
pug9483 <2019136060@koreatech.ac.kr>
2024-04-12 20:39:56 +0900

    spring data jpa을 이용한 fetch join

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -1,11 +1,13 @@
 package study.datajpa.repository;
 
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 import java.util.List;
 
@@ -108,4 +110,6 @@ class MemberJpaRepositoryTest {
 
         assertThat(resultCount).isEqualTo(3);
     }
+
+
 }
